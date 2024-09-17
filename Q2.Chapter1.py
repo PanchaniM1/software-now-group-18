fb51645359dd11245b9cb8fe8ccb949861fcfaae
PanchaniM1 <s382845@students.cdu.edu.au>
2024-09-18 09:01:35 +0930

    Q01Chapter1

--- Q2.Chapter1.py
+++ Q2.Chapter1.py
@@ -13,7 +13,6 @@ if generated_number % 2 == 0:
 # Display the final generated number
 print("The dynamic number generated is:", generated_number)
 
-
 # Step 2: Open the image file
 img = Image.open('chapter1.jpg')
 pixels = img.load()
